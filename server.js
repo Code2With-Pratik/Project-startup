dc4b471a7f103838f6eb21235d05a7bee7d5c2f0
Code2With-Pratik <work.pratik1204@gmail.com>
2025-02-26 10:42:54 +0530

    mongodb connected

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 const express = require("express");
-const path = require("path");
 const app = express();
+const port = 3000;
+const expressLayouts = require("express-ejs-layouts");
+
+const indexRouters = require("./routes/index");
 
 // Set EJS as the templating engine
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views")); // ✅ Correct way to set views path
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout"); // Main layout file
+app.use(expressLayouts);
 
-// Middleware
+// Middleware to serve static files (CSS, JS, Images)
 app.use(express.static("public"));
 
-// Import routes
-const indexRouter = require("./routes/index");
-app.use("/", indexRouter);
+// Connection to MongoDB
+const mongoose = require("mongoose");
+mongoose.connect(process.env.Database_URL, {
+    useNewUrlparser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+
+app.use("/", indexRouters);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
+app.listen(process.env.PORT || 3000)
